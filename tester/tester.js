ef7aed4b2e68c5d36522025b456ed7623ac2c5e5
XadillaX <admin@xcoder.in>
2014-04-01 20:10:19 +0800

    bug

--- tester/tester.js
+++ tester/tester.js
@@ -3,23 +3,30 @@
  */
 var vjsubmitter = require("../");
 
-var submitter = vjsubmitter.createSubmitter("lsu", "USERNAME", "PASSWORD");
-submitter.judge(1000, "C++", '#include "iostream"\n\
-    using namespace std;\
-    int main()\
-    {\
-        int i = 100000000;\
-        while(i--);\
-        int a,b;\
-        cin >> a >> b;\
-        cout << a+b << endl;\
-        return 0;\
-    }',
-    function(err, result) {
-        if(err) {
-            console.log(err.message);
-        } else {
-            console.log(result);
+lsuTest();
+
+/**
+ * Lishui University Online Judge System
+ */
+function lsuTest() {
+    var lsu = vjsubmitter.createSubmitter("lsu", "USERNAME", "PASSWORD");
+    lsu.judge(1000, "C++", '#include "iostream"\n\
+        using namespace std;\
+        int main()\
+        {\
+            int i = 1000000000;\
+            while(i--);\
+            int a,b;\
+            cin >> a >> b;\
+            cout << a+b << endl;\
+            return 0;\
+        }',
+        function(err, result) {
+            if(err) {
+                console.log(err.message);
+            } else {
+                console.log(result);
+            }
         }
-    }
-);
+    );
+}
